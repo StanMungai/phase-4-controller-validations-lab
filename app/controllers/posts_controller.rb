8244777674ec01d8720d4d4a7a5c437286f79b7e
stanford kangethe <stanfordkangethe16@gmail.com>
2022-11-26 08:01:14 +0300

    commit tree acc96b10a50b0c1d9bf6599ce811a85f92a333f6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessed_entity_response
 
   def show
     post = Post.find(params[:id])
@@ -9,7 +10,7 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -18,6 +19,14 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  # def render_unprocessed_entity_respose(invalid)
+  #   render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  # end
+
+  def render_unprocessed_entity_response(invalid)
+    render json: {errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
